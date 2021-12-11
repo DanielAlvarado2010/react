496b143aa911c4e1d360a34218c259a31d4d47ec
Daniel_Alvarado <danielalvarado1498@gmail.com>
2021-12-10 19:27:27 -0600

    Navbar class

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,20 +2,24 @@ import React, { useEffect, useState} from 'react';
 import './Navbar.css';
 
 export default function Navbar() {
-    const [ firstClass, setFirstClass ] = useState("firstClass");
+    const [ itemStyle, setClass ] = useState(0); 
     
-    function classChanger (firstClass) {
-        setFirstClass = 'secondClass';
-        return firstClass;
-    }
+    const classChanger = (change) => setClass(change);
+
+    const clickedOption = (element) => (itemStyle === element ? "active-li" : "" )
+
+    const parrChanger = (element) => (itemStyle=== element ? "active-p" : "deactive-p")
 
     return (
 		<div>
 			<ul>
-				<li className={firstClass}><button onClick={() => classChanger}>Item 1</button></li>
-				<li className={firstClass}><button onClick={() => classChanger}>Item 2</button></li>
-				<li className={firstClass}><button onClick={() => classChanger}>Item 3</button></li>
+				<li><button className={clickedOption(1)} onClick={() => classChanger(1)}>Item 1</button></li>
+				<li><button className={clickedOption(2)} onClick={() => classChanger(2)}>Item 2</button></li>
+				<li><button className={clickedOption(3)} onClick={() => classChanger(3)}>Item 3</button></li>
 			</ul>
+            <p className={parrChanger(1)}>Esto se despliega si se presiona el primer boton</p>
+            <p className={parrChanger(2)}>Si se presiona el segundo boton esto se desplegara</p>
+            <p className={parrChanger(3)}>Ultima opción seleccionada al picar el ultimo boton</p>
 		</div>
 	);
 }
