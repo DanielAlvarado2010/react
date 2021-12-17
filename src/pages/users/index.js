ba018e1e366485c12092abb2c9ad5dd5034a3bae
Daniel_Alvarado <danielalvarado1498@gmail.com>
2021-12-17 17:13:36 -0600

    Rendering user list from firebase

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,5 +1,37 @@
 import React from "react";
 
+import { useState, useEffect } from "react";
+import { getUsers } from "../../services/users";
+
 export default function Users() {
-  return <div>Aqui va a ser mi lista de usuarios</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const get = async () => {
+      const response = await getUsers();
+      let result = Object.keys(response).map((element) => {
+        const { firstName, lastName } = response[element];
+        return {
+          id: element,
+          firstName,
+          lastName,
+        };
+      });
+      setUsers(result);
+    };
+
+    get();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {users.map(({ id, firstName, lastName }) => (
+          <li key={id}>
+            {firstName} {lastName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/services/users.js
+++ src/services/users.js
@@ -13,4 +13,15 @@ const createUser = async (data) => {
   return await response.json();
 };
 
-export { createUser };
+const getUsers = async () => {
+  const response = await fetch(
+    "https://react-14g-default-rtdb.firebaseio.com/users.json"
+  );
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+
+  return data;
+};
+
+export { createUser, getUsers };
